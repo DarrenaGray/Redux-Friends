fa6573ff967b095972cef4ffcea544046fe298ab
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-11 18:01:43 -0400

    Added reducer for fetching friends

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions";
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE, FETCH_FRIENDS_START, FETCH_FRIENDS_SUCCESS, FETCH_FRIENDS_FAILURE } from "../actions";
 
 
 const initialState = {
@@ -32,6 +32,27 @@ const reducer = (state = initialState, action) => {
                 loggingIn: false,
                 error: ''
             }
+        case FETCH_FRIENDS_START:
+            return {
+                ...state,
+                fetchingFriends: true,
+                error: ''
+            }
+        case FETCH_FRIENDS_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                fetchingFriends: false,
+                error: ''
+            }
+        case FETCH_FRIENDS_FAILURE:
+            return {
+                ...state,
+                fetchingFriends: false,
+                error: action.payload
+            }
+        default:
+            return state;
     }
 }
 
