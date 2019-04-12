0cefaeea9e43e42ceb58cdb3d696ff86bf952237
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-12 00:53:46 -0400

    Added a return on axios login request to fix an undefine error when clicking the login button

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -11,17 +11,18 @@ export const FETCH_FRIENDS_FAILURE = "FETCH_FRIEND_FAILURE";
 const baseUrl = 'http://localhost:5000';
 export const login = creds => dispatch => {
     dispatch({ type: LOGIN_START });
-    axios
-        .post(`${baseUrl}/api/login`, creds)
-        .then(res => {
-            console.log(res)
-            localStorage.setItem('token', res.data.payload)
-            dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
-        })
-        .catch(err => {
-            console.log(err)
-            dispatch({ type: LOGIN_FAILURE, payload: err })
-        })
+    return (
+        axios 
+            .post(`${baseUrl}/api/login`, creds)
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data.payload)
+                dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        );
 }
 
 export const getFriends = () => dispatch => {
