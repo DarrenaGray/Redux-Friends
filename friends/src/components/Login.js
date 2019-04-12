26e7d965a0c58db2d0306c4c5b78e142c1a9432d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-12 00:56:01 -0400

    Called login function to form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -21,17 +21,17 @@ class Login extends React.Component  {
     }
 
     login = e => {
+        console.log('Logging in ...', this.state.credentials)
         e.preventDefault();
-        this.props.login(this.state.credentials)
-        .then(() => {
-            this.history.push('/protected');
-        })
+        this.props
+        .login(this.state.credentials)
+        .then(() => this.props.history.push('/protected'))
     }
 
     render() {
         return (
             <div className="login">
-                <form>
+                <form onSubmit={this.login}>
                     <input 
                         type="text"
                         name="username"
@@ -54,6 +54,7 @@ class Login extends React.Component  {
 }
 
 const mapStateToProps = state => {
+    console.log(state)
     return {
         loggingIn: state.loggingIn
     }
