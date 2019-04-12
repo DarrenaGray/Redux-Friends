28a32c6034218a53a7acf6c0d4abe5813d148d0b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-12 02:04:31 -0400

    Reducer added for deleting friend

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE, FETCH_FRIENDS_START, FETCH_FRIENDS_SUCCESS, FETCH_FRIENDS_FAILURE } from "../actions";
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE, FETCH_FRIENDS_START, FETCH_FRIENDS_SUCCESS, FETCH_FRIENDS_FAILURE, DELETE_FRIEND_START, DELETE_FRIEND_SUCCESS } from "../actions";
 
 
 const initialState = {
@@ -49,6 +49,19 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 fetchingFriends: false,
+                error: ''
+            }
+        case DELETE_FRIEND_START:
+            return {
+                ...state,
+                fetchingFriends: false,
+                deletingFriend: true,
+                error: ''
+            }
+        case DELETE_FRIEND_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
                 error: ''
             }
         default:
