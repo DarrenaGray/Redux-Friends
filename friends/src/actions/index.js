38ba7fb49a53e5c7d234bfc961ff348f1dc7f95c
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-12 01:59:55 -0400

    Added action creator for deleting friend

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -8,6 +8,10 @@ export const FETCH_FRIENDS_START = "FETCH_FRIEND_START";
 export const FETCH_FRIENDS_SUCCESS = "FETCH_FRIEND_SUCCESS";
 export const FETCH_FRIENDS_FAILURE = "FETCH_FRIEND_FAILURE";
 
+export const DELETE_FRIEND_START = "DELETE_FRIEND_START";
+export const DELETE_FRIEND_SUCCESS = "DELETE_FRIEND_SUCCESS";
+export const DELETE_FRIEND_FAILURE = "DELETE_FRIEND_FAILURE";
+
 const baseUrl = 'http://localhost:5000';
 export const login = creds => dispatch => {
     dispatch({ type: LOGIN_START });
@@ -38,5 +42,21 @@ export const getFriends = () => dispatch => {
         .catch(err => {
             console.log(err.response)
             dispatch({ type: FETCH_FRIENDS_FAILURE, payload: err.response })
+        });
+}
+
+export const deleteFriend = id => dispatch => {
+    dispatch({ type: DELETE_FRIEND_START })
+    axios
+        .delete(`${baseUrl}/api/friends/${id}`, {
+            headers: { Authorization: localStorage.getItem('token') }
+        })
+        .then(res => {
+            console.log(res)
+            dispatch({ type: DELETE_FRIEND_SUCCESS, payload: res.data });
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch({ type: DELETE_FRIEND_FAILURE, payload: err })
         })
 }
